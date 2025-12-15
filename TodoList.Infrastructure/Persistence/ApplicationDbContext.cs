c7664f5f1ab80d391d2b49b112653b6f1f3c3c18
codebyallan <allan.pereira.dev@proton.me>
2025-12-15 15:48:15 -0300

    feat(infrastructure): Add unique index to User.Username
    This commit Configures a unique index on the 'Username' column of the 'Users' table.
    
    This ensures data integrity by preventing the creation of multiple users with the same username.

--- TodoList.Infrastructure/Persistence/ApplicationDbContext.cs
+++ TodoList.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -8,5 +8,11 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     public DbSet<TodoItem> TodoItems { get; set; }
     public DbSet<User> Users { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Username)
+            .IsUnique();
+    }
 
 }
